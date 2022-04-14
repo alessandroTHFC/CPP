048986c5d18bd266a31ea7c5eaea0d2b8a3857b1
Alessandro Pangrazio <alessandropangrazio@live.com>
2022-04-14 17:17:37 +0930

    ex02 Complete

---
+++ moduleTwo/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void) {
+
+    std::string     stringVar = "HI THIS IS BRAIN";
+    std::string*    stringPtr = &stringVar;
+    std::string&    stringRef = stringVar;
+
+    std::cout << "Memory address for stringVar is " << &stringVar << std::endl;
+    std::cout << "Memory address for stringPtr is " << stringPtr << std::endl;
+    std::cout << "Memory address for stringRef is " << &stringRef << std::endl;
+
+    std::cout << "Value for stringVar is " << stringVar << std::endl;
+    std::cout << "Value for stringPtr is " << *stringPtr << std::endl;
+    std::cout << "Value for stringPtr is " << stringRef << std::endl;
+}
