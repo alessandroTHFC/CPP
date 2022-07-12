8f2d66cbe79f8ef9cdd7ce16ff288312197ec534
alessandroTHFC <apangraz@SRA-LT-2033.sra.com.au>
2022-07-12 16:37:12 +0930

    starting module 8

---
+++ moduleEight/ex00/easyFind.hpp
@@ -0,0 +1,9 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+///Range based for loop. 
+///for_each
+///iterators
+///c ++ algorithms
+
+#endif
--- moduleSeven/ex02/main.cpp
+++ moduleSeven/ex02/main.cpp
@@ -1,27 +1,31 @@
 #include "Array.hpp"
 
 int main(void) {
-
-    hipHopArray<int> firstOne(10);
+    ///testing with array of integers
+    hipHopArray<int> firstOne(10);///array size of 10
     firstOne.setArray(10);
     firstOne.setArray(69);
     firstOne.setArray(1993);
     firstOne.printArrayContents();
     std::cout << "<<<<<<<<<<<>>>>>>>>>>" << std::endl;
+    ///testing with array of strings
     hipHopArray<std::string> secondOne(5);
     secondOne.setArray("This ain't got shit to do with shampoo, but watch your head n shoulder");
     secondOne.setArray("Brother older bold enough to fold ya, yo I told ya");
     secondOne.printArrayContents();
     std::cout << "<<<<<<<<<<<>>>>>>>>>>" << std::endl;
+    ///overloading the assign operator
     hipHopArray<std::string> thirdOne;
     thirdOne = secondOne;
     thirdOne.printArrayContents();
-
+    ///try block using the overloaded[], using a non valid number will be caught by the exception.
     try {
         std::cout << thirdOne[9] << std::endl;
     }
     catch (std::exception &e) {
         std::cout << e.what() << std::endl;
     }
+
+    ///Above try block will fail as trying to access array index 9 when the array is only 5 long.
     return (0);
 }
